effe911a7aa2f1c6218171a6e0d22f5e0402acb0
tony_cox <tonycox@bk.ru>
2016-02-21 02:59:21 +0400

    more lines

--- Calculator/src/main/java/interfaces/InterfaceCalc.java
+++ Calculator/src/main/java/interfaces/InterfaceCalc.java
@@ -1,12 +1,13 @@
 package interfaces;
 
-/**
- * Created by Компик on 10.02.2016.
- */
 public interface InterfaceCalc {
     double schet();
+
     double plus();
+
     double minus();
+
     double umn();
+
     double del();
 }
